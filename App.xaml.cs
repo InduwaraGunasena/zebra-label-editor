6d4829f442bf3245c93a3f20c0ea86bf852c572e
Induwara <induwaragunasena.sl@gmail.com>
2026-01-03 13:49:46 +0530

    Updated upto print/save interface

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,6 +2,10 @@
 using System.Data;
 using System.Windows;
 
+using zebra_label_editor.Services.Implementations;
+using zebra_label_editor.Services.Interfaces;
+using zebra_label_editor.ViewModels;
+
 namespace zebra_label_editor
 {
     /// <summary>
@@ -9,6 +13,32 @@ namespace zebra_label_editor
     /// </summary>
     public partial class App : Application
     {
-    }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // 1. Create Services
+            IZplService zplService = new ZplService();
+            IExcelService excelService = new ExcelService();
 
+            // 2. Create Child ViewModels
+            var importVm = new ImportViewModel(zplService);
+            var mappingVm = new MappingViewModel(excelService);
+            var selectionVm = new SelectionViewModel();
+            var printConfigVm = new PrintConfigViewModel(); // Assumes empty constructor for now
+            var saveConfigVm = new SaveConfigViewModel();   
+
+            // 3. Create MainViewModel (The Traffic Cop)
+            var mainVm = new MainViewModel(
+                importVm, 
+                mappingVm, 
+                selectionVm,
+                printConfigVm,
+                saveConfigVm
+                );
+
+            // 4. Create and Show the Main Window
+            MainWindow window = new MainWindow();
+            window.DataContext = mainVm; // Give the Brain to the Window
+            window.Show();
+        }
+    }
 }
